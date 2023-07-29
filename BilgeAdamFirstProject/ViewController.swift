9bf90c8f62049cd3b31f2f3ce1fd6f09b5e5dc9c
kubra demirkaya <kubrademirkaya3@gmail.com>
2023-07-29 21:43:53 +0300

    Initial Commit

---
+++ BilgeAdamFirstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BilgeAdamFirstProject
+//
+//  Created by Kübra Demirkaya on 29.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
